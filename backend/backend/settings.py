b158a6df99b0ed3dc87f022138eb11b1d8f40360
sreerang k <sreerangk77@gmail.com>
2022-12-30 22:41:44 +0530

    complete backend dummy data testing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
